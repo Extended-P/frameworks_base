60cf4427ab2ee3fdbb5342cd912ec9791013516d
Keith Mok <kmok@cyngn.com>
2016-02-04 20:10:00 +0100

    SystemUI: add caffeine qs tile
    Adapt for Oreo by @xyyx
    abc edits: switch to Infinite Caffeine version by @iskandar1023
    
    Change-Id: I03bfc8c9bae4a4be92224c1eccbae0d58b0b7769

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -26,6 +26,7 @@ import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
+import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
@@ -107,6 +108,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new ImmersiveTile(mHost);
             case "soundsearch":
                 return new SoundSearchTIle(mHost);
+            case "caffeine":
+                return new CaffeineTile(mHost);
         }
 
         // Intent tiles.
