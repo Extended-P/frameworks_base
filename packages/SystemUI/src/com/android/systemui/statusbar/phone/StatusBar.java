64da24f03a02b4e8d07e2dd71403de58f014dba6
ezio84 <brabus84@gmail.com>
2017-09-01 15:28:27 +0200

    Double tap to sleep on lockscreen [1/2]
    Change-Id: Icd8de7c5512362f22dcea9c3946347a1f0c13aba

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5528,6 +5528,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -5543,8 +5546,10 @@ public class StatusBar extends SystemUI implements DemoMode,
                updateAccents();
 
             } else if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN))) {
-               setStatusBarWindowViewOptions();
+                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN))
+                    || uri.equals(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN))) {
+                setStatusBarWindowViewOptions();
             }
         }
 
